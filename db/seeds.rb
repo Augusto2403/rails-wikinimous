57360e58e8e33a2dd0c63af1e177c78387dedc8b
Augusto Leone <augustoleone@MacBook-Pro-de-Augusto-3.local>
2022-05-18 09:02:27 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  new_article = Article.create([{ title: Faker::Beer.name, content: Faker::Beer.brand}])
+  new_article.save
+end
